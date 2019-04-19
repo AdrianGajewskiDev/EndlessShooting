eb2720cedf07da5050d39f3cef1810aad54c7392
adrian gajewski <adigtc001@outlook.com>
2019-04-19 14:54:04 +0200

    Added a gun icons and script to controll With icon should be displayed, depending on gun type

---
+++ Endless Shooting/Assets/Scripts/UI/UIIconsController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIIconsController : MonoBehaviour
+{
+    public Image HandGunIcon;
+    public Image SMGGunIcon;
+
+    //  Update is called once per frame
+    void Update()
+    {
+        switch(UIAmmoController.Type)
+        {
+            case GunType.HandGun:
+            {
+                SMGGunIcon.gameObject.SetActive(false);
+                HandGunIcon.gameObject.SetActive(true);
+            }break;
+
+            case GunType.SMGGun:
+            {
+                HandGunIcon.gameObject.SetActive(false);
+                SMGGunIcon.gameObject.SetActive(true);
+            }break;
+        }
+    }
+}
