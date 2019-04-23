ea76f70ad3a91bfd41a10ed81c4806610e9f8903
adrian gajewski <adigtc001@outlook.com>
2019-04-23 13:32:28 +0200

    Added a Aiming script

---
+++ Endless Shooting/Assets/Scripts/Gun Mechanics/Aiming.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aiming : MonoBehaviour
+{
+    bool IsAiming = false;
+    public float aimSpeed;
+    public Vector3 NormalPosition;
+    public Vector3 AimPosition;
+    
+
+    void Update()
+    {
+        InputController.Right_Mouse = Input.GetMouseButton(1);
+        Aim();
+    }
+
+    void Aim()
+    {
+        if( InputController.Right_Mouse && IsAiming == false)
+        {
+            transform.localPosition = AimPosition;
+            IsAiming = true;
+        }
+
+        if(!InputController.Right_Mouse && IsAiming == true)
+        {
+            transform.localPosition = NormalPosition;
+            IsAiming = false;
+        }
+    }
+}
--- Endless Shooting/Assets/Scripts/Input/InputController.cs
+++ Endless Shooting/Assets/Scripts/Input/InputController.cs
@@ -5,5 +5,6 @@ using UnityEngine;
 public class InputController : MonoBehaviour
 {
     public static bool Left_Mouse;
+    public static bool Right_Mouse;
     public static bool Reload_Button;
 }
